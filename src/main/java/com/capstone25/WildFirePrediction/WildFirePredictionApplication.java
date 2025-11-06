81f7e6c4b017cca579012fcde0784ee46edd1159
riadan710 <riadan710@gmail.com>
2025-11-06 19:33:55 +0900

    Initial commit

---
+++ src/main/java/com/capstone25/WildFirePrediction/WildFirePredictionApplication.java
@@ -0,0 +1,13 @@
+package com.capstone25.WildFirePrediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildFirePredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildFirePredictionApplication.class, args);
+	}
+
+}
