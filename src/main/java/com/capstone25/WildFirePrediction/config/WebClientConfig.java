d535e636a6ad7a51afee889f00f6e31924d7fb1a
riadan710 <riadan710@gmail.com>
2025-12-09 16:18:05 +0900

    feat: 공공api 관련 config 추가
    yml에서 값 가져옴

--- src/main/java/com/capstone25/WildFirePrediction/config/WebClientConfig.java
+++ src/main/java/com/capstone25/WildFirePrediction/config/WebClientConfig.java
@@ -5,6 +5,7 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -12,10 +13,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebClientConfig {
 
+    // yml을 매핑한 설정 객체 주입
+    private final SafetyDataProperties safetyDataProperties;
+
     @Bean
-    public WebClient webClient() {
+    public WebClient safetyDataWebClient() {
         // HttpClient 설정: 연결 타임아웃과 응답 타임아웃 설정
         HttpClient httpClient = HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)  // 연결 타임아웃: 5초
@@ -24,7 +29,9 @@ public class WebClientConfig {
                         .addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS))  // 읽기 타임아웃
                         .addHandlerLast(new WriteTimeoutHandler(10, TimeUnit.SECONDS)));  // 쓰기 타임아웃
 
+        // 재난안전데이터 공유플랫폼 전용 WebClient
         return WebClient.builder()
+                .baseUrl(safetyDataProperties.getBaseUrl())  // 공통 base URL 설정
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
