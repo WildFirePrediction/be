9704818182df4c80ce2614d324f5504bf6d9635b
riadan710 <riadan710@gmail.com>
2025-12-10 16:39:54 +0900

    feat: 재난문자 컨트롤러 구현

---
+++ src/main/java/com/capstone25/WildFirePrediction/controller/EmergencyMessageController.java
@@ -0,0 +1,26 @@
+package com.capstone25.WildFirePrediction.controller;
+
+import com.capstone25.WildFirePrediction.global.ApiResponse;
+import com.capstone25.WildFirePrediction.service.EmergencyMessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emergency-messages")
+@RequiredArgsConstructor
+public class EmergencyMessageController {
+
+    private final EmergencyMessageService emergencyMessageService;
+
+    @PostMapping("/load-today")
+    @Operation(summary = "오늘 재난문자 데이터 수집",
+            description = "오늘 날짜 기준으로 공공 API에서 재난문자 데이터를 조회하여, " +
+                    "이미 저장된 일련번호를 제외한 신규 데이터만 DB에 저장합니다.")
+    public ApiResponse<String> loadTodaysMessages() {
+        emergencyMessageService.loadTodaysEmergencyMessages();
+        return ApiResponse.onSuccess("오늘 재난문자 수집 및 저장 완료");
+    }
+}
