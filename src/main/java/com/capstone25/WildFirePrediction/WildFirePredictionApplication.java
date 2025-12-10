c2ea51b1b3d7407c301892dd75c664e44d5b673d
riadan710 <riadan710@gmail.com>
2025-12-10 15:43:56 +0900

    deploy: Docker 컨테이너 time KST 변경

--- src/main/java/com/capstone25/WildFirePrediction/WildFirePredictionApplication.java
+++ src/main/java/com/capstone25/WildFirePrediction/WildFirePredictionApplication.java
@@ -1,5 +1,7 @@
 package com.capstone25.WildFirePrediction;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class WildFirePredictionApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WildFirePredictionApplication.class, args);
